0b9db39c594352a0bdc6af2ed2e43aeabc6da52f
user-iot <alda.martin.munoz@alumnos.upm.es>
2018-12-13 21:35:04 +0100

    prueba fsm

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-CONTIKI=../../../../../../
+CONTIKI=../../../../../../../
 
 TARGET = mikro-e
 CONTIKI_WITH_IPV6 = 1
@@ -8,7 +8,7 @@ VERSION? = $(shell git describe --abbrev=4 --dirty --always --tags)
 
 APPS += letmecreateiot
 
-CONTIKI_SOURCEFILES += mcp3004.c
+CONTIKI_SOURCEFILES += mcp3004.c fsm.c
 
 CFLAGS += -DDEBUG_IP=fe80:0000:0000:0000:0019:f5ff:fe89:1e32
 CFLAGS += -DVERSION=$(VERSION)
--- main.c
+++ main.c
@@ -16,7 +16,59 @@
 #include "common.h"
 #include "mcp3004.h"
 
+
+#include "fsm.h"
+
 #define SERVER_IP_ADDR "fe80:0000:0000:0000:0020:f5f8:f329:1234"
+
+
+//Funciones de salida. Hacen una acción. Devuelven void
+
+void send_alert(fsm_t* fsm){
+    printf("Plato listo para llevar \n");
+    leds_on(LED1);
+    //envío paquete UDP
+    //udp_packet_send(conn, buffer, strlen(buffer));
+    leds_on(LED1);
+    //PROCESS_WAIT_UDP_SENT();
+}
+
+void clear_alert(fsm_t* fsm){
+    printf("Nada para llevar \n");
+    //envío paquete UDP
+    leds_off(LED1);
+}
+
+//Funciones de lectura de sensores para comprobar el cambio de estado. Devuelven 1 si queremos cambiar, 0 si no
+
+int check_sensor(fsm_t* fsm){
+    printf("comprueba valor sensor IR \n");
+    static float value = 0.0f;
+    static uint8_t channel =1;
+    mcp3004_read_channel(channel, &value);
+    printf("VALUE: %f\n", value);
+    if(value > 2){ //boton izquierda
+        printf("hay plato \n");
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+int check_sensor_none(fsm_t* fsm){
+    printf("comprueba valor sensor IR \n");
+    static float value = 0.0f;
+    static uint8_t channel =1;
+    mcp3004_read_channel(channel, &value);
+    printf("VALUE: %f\n", value);
+    if(value < 2){ //boton izquierda
+        printf("no hay plato \n");
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
 
 PROCESS(main_process, "Main process");
 AUTOSTART_PROCESSES(&main_process);
@@ -30,30 +82,38 @@ PROCESS_THREAD(main_process, ev, data)
         MEMB(appdata, struct idappdata, MAXDATASIZE);
 
         static char buffer[MAXDATASIZE + 1];
-        static float value = 0.0f;
         static struct etimer et;
         static struct idappdata* operacion;
         static struct idappdata* resultado;
         static struct uip_ip_hdr metadata;
         static struct uip_udp_conn* conn;
-        static uint8_t channel;
         static uint8_t i;
         static uint8_t id;
-        static uint16_t minor, major;  
+        static uint16_t minor, major; 
+
+        static fsm_t* fsm;
+
+        enum fsm_state { EMPTY, DETECTED };
+        
+        static fsm_trans_t sensor_ir[] = {
+            {EMPTY,     check_sensor,    DETECTED, send_alert},
+            {DETECTED,  check_sensor_none,   EMPTY,    clear_alert},
+            {-1, NULL, -1, NULL},
+        }; 
 
         operacion = memb_alloc(&appdata);
         resultado = memb_alloc(&appdata);
 
-        PRINTF("=====Start=====\n");
+        printf("=====Start=====\n");
 
         if(spi_init() < 0 ||
             spi_set_mode(MIKROBUS_1, SPI_MODE_3) < 0)
         {
-            PRINTF("SPI init failed\n");
+            printf("SPI init failed\n");
             return 1;
         }
 
-        PRINTF("SPI init passed\n");
+        printf("SPI init passed\n");
 
         int flashes = 2;
         while(flashes--) {
@@ -66,7 +126,7 @@ PROCESS_THREAD(main_process, ev, data)
 
         if(ipv6_add_address(SERVER_IP_ADDR, NULL, NETWORK_INFINITE_LIFETIME) < 0)
         {
-            PRINTF("Failed to set IPV6 address\n");
+            printf("Failed to set IPV6 address\n");
             return 1;
         }
         ipv6_add_default_route(IP6_CI40, NETWORK_INFINITE_LIFETIME);
@@ -82,14 +142,14 @@ PROCESS_THREAD(main_process, ev, data)
             leds_toggle(LED1);
         }
 
-        PRINTF("Quien soy?\n");
+        printf("Quien soy?\n");
 
         memset(operacion->data, '\0', MAXDATASIZE - ID_HEADER_LEN);
         operacion->op = OP_WHOAMI_IR;
         operacion->id = 0;
         operacion->len = strlen(operacion->data);
 
-        PRINTF("OP: 0x%X\nID: %ld\nLen: %ld\nData: %s\n", operacion->op, operacion->id, operacion->len, operacion->data);
+        printf("OP: 0x%X\nID: %ld\nLen: %ld\nData: %s\n", operacion->op, operacion->id, operacion->len, operacion->data);
 
         leds_on(LED1);
         leds_on(LED2);
@@ -102,29 +162,31 @@ PROCESS_THREAD(main_process, ev, data)
         memset(buffer, '\0', MAXDATASIZE + 1);
         udp_packet_receive(buffer, MAXDATASIZE, &metadata);
 
-        PRINTF("buffer: %s\n", buffer);
+        printf("buffer: %s\n", buffer);
 
-        PRINTF("Starting loop\n");
+
+        fsm = fsm_new(sensor_ir);
+
+        printf("********FSM CREADA********\n");
+        leds_off(LED2); 
+
+
+
+        printf("Starting loop\n");
 
         while(1)
         {
-            /*channel = 1;
-            mcp3004_read_channel(channel, &value);
+            etimer_set(&et, CLOCK_SECOND);
 
-            major = (int)value;
-            minor = (int)((value - (float)major) * 100.0f);
+            fsm_fire(fsm);
 
-            PRINTF("Valor: %i.%02i V\n", major, minor);
+            PROCESS_WAIT_EVENT();
+
+            /*
 
             sprintf(buffer, "Voltage channel%ld: %i.%02i\n", channel, major, minor);
+*/
 
-            udp_packet_send(conn, buffer, strlen(buffer));
-            leds_on(LED1);
-            PROCESS_WAIT_UDP_SENT();
-            leds_off(LED1);*/
-
-            etimer_set(&et, CLOCK_SECOND);
-            PROCESS_WAIT_EVENT();
         }
 
         spi_release();

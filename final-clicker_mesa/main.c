48c71446299f0e1f1789a4c5141b555b15e15d16
Carlos Moro <carlosmoro95@gmail.com>
2019-01-17 19:11:38 +0100

    (Mesa) Código bonito y algunos comentarios

--- final-clicker_mesa/main.c
+++ final-clicker_mesa/main.c
@@ -4,33 +4,32 @@
 #include <contiki.h>
 #include <contiki-net.h>
 
-#include "sys/clock.h"
 #include "dev/leds.h"
+#include "lib/memb.h"
+#include "sys/clock.h"
 
 #include "letmecreate/core/network.h"
 #include "letmecreate/core/debug.h"
 #include "letmecreate/core/common.h"
 #include "letmecreate/core/spi.h"
-#include "lib/memb.h"
+#include "letmecreate/click/led_matrix.h"
 #include "mqtt.h"
 
 #include "common.h"
 #include "fsm.h"
 
-#include "letmecreate/click/led_matrix.h"
-
 #include <lib/sensors.h>
 #include <pic32_gpio.h>
 
-#define UMBRAL 2
-#define N_PLATOS 4
+#define N_BUTTONS   2
+#define N_COL_LEDS  8
 
 #define FLAG_BUTTON_1 1
 #define FLAG_BUTTON_2 2
 
-static struct timer debouncetimer[2];
-static int button_pressed[2];
-static uint8_t dibujos[7][8] = { 
+static struct mqtt_connection mqtt_conn;
+static uint8_t buttons_flags_status = 0; //0 apagado, 1 pulsado [0] -> boton1, [1]->boton2
+static uint8_t dibujos[7][N_COL_LEDS] = { 
     { 0x00, 0x00, 0x00, 0x18, 0x18, 0x00, 0x00, 0x00 },  // Config 1
     { 0x00, 0x00, 0x3C, 0x24, 0x24, 0x3C, 0x00, 0x00 },  // Config 2
     { 0x00, 0x7E, 0x42, 0x42, 0x42, 0x42, 0x7E, 0x00 },  // Config 3
@@ -40,82 +39,66 @@ static uint8_t dibujos[7][8] = {
     { 0x00, 0x22, 0x41, 0x55, 0x55, 0x55, 0x3E, 0x14 }   // Bill asked
 };
 
-
-static uint8_t buttons_flags_status = 0; //0 apagado, 1 pulsado [0] -> boton1, [1]->boton2
-
 MEMB(appdata, struct idappdata, MAXDATASIZE);
-static struct mqtt_connection mqtt_conn;
-
-uint8_t convert_values(float* values, int n)
-{
-    uint8_t resp = 0;
-    uint8_t i;
-
-    for (i = 0; i < n; i++) {
-        if (values[i] > UMBRAL) {
-            resp |= (1 << i);
-        }
-    }
-
-    return resp;
-}
-
-
-void init_buttons(){
-    //inicialización botón izquierda
-    ANSELEbits.ANSE7=0;
-    TRISEbits.TRISE7=1;
-    LATEbits.LATE7=0;
-
-    //inicialización botón derecha
-    ANSELBbits.ANSB0=0;
-    TRISBbits.TRISB0=1;
-    LATBbits.LATB0=0;    
-}
 
 /*---------------------------------------------------------------------------*/
-static void button_isr(int button_id, int flag)
+void init_buttons()
 {
-  if (!button_pressed[button_id]) {
-    /*
-     * If timer is still running, it means the button has just been released and
-     * it is a false notification due to bouncing
-     */
-    if (timer_expired(&debouncetimer[button_id])) {
-      button_pressed[button_id] = 1;
-      /* Set a timer for 100ms to ignore false notifications due to bouncing */
-      timer_set(&debouncetimer[button_id], CLOCK_SECOND / 10);
-      /* Notify processes that button has been pressed */
-      buttons_flags_status |= flag;
-    }
-  } else {
-    /*
-     * If timer is still running, it means the button has just been pressed and
-     * it is a false notification due to bouncing
-     */
-    if (timer_expired(&debouncetimer[button_id])) {
-      button_pressed[button_id] = 0;
-      /* Set a timer for 100ms to ignore false notifications due to bouncing */
-      timer_set(&debouncetimer[button_id], CLOCK_SECOND / 10);
-    }
-  }
-}
+    //inicialización botón izquierda
+    ANSELEbits.ANSE7 = 0;
+    TRISEbits.TRISE7 = 1;
+    LATEbits.LATE7 = 0;
 
-static void
-button_callback(void)
+    //inicialización botón derecha
+    ANSELBbits.ANSB0 = 0;
+    TRISBbits.TRISB0 = 1;
+    LATBbits.LATB0 = 0;    
+}
+/*---------------------------------------------------------------------------*/
+void button_isr(int button_id, int flag)
 {
-    if(PORTBbits.RB0 == 0){ //boton derecha
-        button_isr(1, FLAG_BUTTON_2);
+    static struct timer debouncetimer[N_BUTTONS];
+    static uint8_t button_pressed[N_BUTTONS];
+
+    if (!button_pressed[button_id]) {
+        /*
+        * If timer is still running, it means the button has just been released and
+        * it is a false notification due to bouncing
+        */
+        if (timer_expired(&debouncetimer[button_id])) {
+        button_pressed[button_id] = 1;
+            /* Set a timer for 100ms to ignore false notifications due to bouncing */
+            timer_set(&debouncetimer[button_id], CLOCK_SECOND / 10);
+            /* Pone a 1 el flag del botón pulsado */
+            buttons_flags_status |= flag;
+        }
+    } else {
+        /*
+        * If timer is still running, it means the button has just been pressed and
+        * it is a false notification due to bouncing
+        */
+        if (timer_expired(&debouncetimer[button_id])) {
+            button_pressed[button_id] = 0;
+            /* Set a timer for 100ms to ignore false notifications due to bouncing */
+            timer_set(&debouncetimer[button_id], CLOCK_SECOND / 10);
+        }
     }
-    if(PORTEbits.RE7 == 0){ //boton izquierda
-        button_isr(0, FLAG_BUTTON_1);
-    } 
-
 }
-
+/*---------------------------------------------------------------------------*/
+void button_callback(void)
+{
+    if (PORTBbits.RB0 == 0) {  // Si se pulsa boton derecha
+        button_isr(1, FLAG_BUTTON_2);  // Pone a 1 su flag
+    }
+    if (PORTEbits.RE7 == 0) {  // Si se pulsa boton izquierda
+        button_isr(0, FLAG_BUTTON_1);  // Pone a 1 su flag
+    }
+}
+/*---------------------------------------------------------------------------*/
 void send_alert_occupated(fsm_t* fsm)
 {
     static struct idappdata* envio;
+    static uint16_t mid = 0;
     envio = memb_alloc(&appdata);
 
     leds_on(LED1);
@@ -124,8 +107,8 @@ void send_alert_occupated(fsm_t* fsm)
 
     sprintf(envio->data, "Mesa %d ocupada", fsm->id_mesa & 0x3F);
     envio->op = OP_MESA_OCUPADA;
-    envio->id = fsm->id_msg + (fsm->id_mesa << 8);
-    uint16_t mid = envio->id;
+    envio->id = (fsm->id_mesa << 8) + fsm->id_msg;  // El primer byte se corresponde con el id de la mesa y el segundo con el nº de mensaje enviado
+    mid = envio->id;
     envio->len = strlen(envio->data);
 
     mqtt_publish(&mqtt_conn, &mid, "restaurante/mesa/ocupada", (char*) envio, ID_HEADER_LEN + envio->len, 1, 0);
@@ -137,10 +120,11 @@ void send_alert_occupated(fsm_t* fsm)
     memb_free(&appdata, envio);
     leds_off(LED1);
 }
-
+/*---------------------------------------------------------------------------*/
 void send_alert_empty(fsm_t* fsm)
 {
     static struct idappdata* envio;
+    static uint16_t mid = 0;
     envio = memb_alloc(&appdata);
 
     leds_on(LED1);
@@ -149,8 +133,8 @@ void send_alert_empty(fsm_t* fsm)
 
     sprintf(envio->data, "Mesa %d vaciada", fsm->id_mesa & 0x3F);
     envio->op = OP_MESA_VACIA;
-    envio->id = fsm->id_msg + (fsm->id_mesa << 8);
-    uint16_t mid = envio->id;
+    envio->id = (fsm->id_mesa << 8) + fsm->id_msg;  // El primer byte se corresponde con el id de la mesa y el segundo con el nº de mensaje enviado
+    mid = envio->id;
     envio->len = strlen(envio->data);
 
     mqtt_publish(&mqtt_conn, &mid, "restaurante/mesa/vaciada", (char*) envio, ID_HEADER_LEN + envio->len, 1, 0);
@@ -162,10 +146,11 @@ void send_alert_empty(fsm_t* fsm)
     memb_free(&appdata, envio);
     leds_off(LED1);
 }
-
+/*---------------------------------------------------------------------------*/
 void send_alert_bill(fsm_t* fsm)
 {
     static struct idappdata* envio;
+    static uint16_t mid = 0;
     envio = memb_alloc(&appdata);
 
     leds_on(LED1);
@@ -174,8 +159,8 @@ void send_alert_bill(fsm_t* fsm)
 
     sprintf(envio->data, "Mesa %d cuenta pedida", fsm->id_mesa & 0x3F);
     envio->op = OP_MESA_CUENTA;
-    envio->id = fsm->id_msg + (fsm->id_mesa << 8);
-    uint16_t mid = envio->id;
+    envio->id = (fsm->id_mesa << 8) + fsm->id_msg;  // El primer byte se corresponde con el id de la mesa y el segundo con el nº de mensaje enviado
+    mid = envio->id;
     envio->len = strlen(envio->data);
 
     mqtt_publish(&mqtt_conn, &mid, "restaurante/mesa/cuenta", (char*) envio, ID_HEADER_LEN + envio->len, 1, 0);
@@ -187,10 +172,11 @@ void send_alert_bill(fsm_t* fsm)
     memb_free(&appdata, envio);
     leds_off(LED1);
 }
-
+/*---------------------------------------------------------------------------*/
 void send_alert_waiter_call(fsm_t* fsm)
 {
     static struct idappdata* envio;
+    static uint16_t mid = 0;
     envio = memb_alloc(&appdata);
 
     leds_on(LED1);
@@ -199,8 +185,8 @@ void send_alert_waiter_call(fsm_t* fsm)
 
     sprintf(envio->data, "Mesa %d llama camarero", fsm->id_mesa & 0x3F);
     envio->op = OP_MESA_LLAMA;
-    envio->id = fsm->id_msg + (fsm->id_mesa << 8);
-    uint16_t mid = envio->id;
+    envio->id = (fsm->id_mesa << 8) + fsm->id_msg;  // El primer byte se corresponde con el id de la mesa y el segundo con el nº de mensaje enviado
+    mid = envio->id;
     envio->len = strlen(envio->data);
 
     mqtt_publish(&mqtt_conn, &mid, "restaurante/mesa/llamada", (char*) envio, ID_HEADER_LEN + envio->len, 1, 0);
@@ -211,34 +197,36 @@ void send_alert_waiter_call(fsm_t* fsm)
     led_matrix_click_set(dibujos[5]);
     leds_off(LED1);
 }
-
-//Funciones de lectura de sensores para comprobar el cambio de estado. Devuelven 1 si queremos cambiar, 0 si no
+/*---------------------------------------------------------------------------*/
+/* 
+ * Funciones de lectura de sensores para comprobar el cambio de estado.
+ * Devuelven 1 si queremos cambiar, 0 si no
+ *
+ */
 int check_button1(fsm_t* fsm){
-    if((buttons_flags_status & 1) == 1) {
+    if ((buttons_flags_status & 1) == 1) {
         buttons_flags_status = 0;
         return 1;
-    }else{
+    } else {
         return 0;
     }
 }
-
-int check_button2(fsm_t* fsm){
-    if((buttons_flags_status & 2) == 2) {
+/*---------------------------------------------------------------------------*/
+int check_button2(fsm_t* fsm) {
+    if ((buttons_flags_status & 2) == 2) {
         buttons_flags_status = 0;
         return 1;
-    }else{
+    } else {
         return 0;
     }
 }
-
-
-static void
-mqtt_event(struct mqtt_connection *m, mqtt_event_t event, void *data)
+/*---------------------------------------------------------------------------*/
+/* Función necesaria para usar MQTT */
+void mqtt_event(struct mqtt_connection *m, mqtt_event_t event, void *data)
 {
     
 }
-
-
+/*---------------------------------------------------------------------------*/
 PROCESS(main_process, "Main process");
 AUTOSTART_PROCESSES(&main_process);
 /*---------------------------------------------------------------------------*/
@@ -254,7 +242,8 @@ PROCESS_THREAD(main_process, ev, data)
         static struct idappdata* resultado;
         static struct uip_ip_hdr metadata;
         static struct uip_udp_conn* conn;
-        static uint8_t i;
+        static uint8_t flashes = 0;
+        static uint8_t i = 0;
         static uint8_t id_clicker = 0;
         static uint8_t id_msg = 0;
         static uint8_t num_mesa = 0;
@@ -276,25 +265,27 @@ PROCESS_THREAD(main_process, ev, data)
 
         PRINTF("=====Start=====\n");
 
+        // CONFIGURACIÓN SPI
         if(spi_init() < 0 ||
             spi_set_mode(MIKROBUS_1, SPI_MODE_3) < 0)
         {
             PRINTF("SPI init failed\n");
-            return 1;
+            PROCESS_EXIT();
         }
 
-        PRINTF("SPI init passed\n");
-        led_matrix_click_set(dibujos[0]);
+        PRINTF("SPI init passed\n");    
 
-
+        // CONFIGURACIÓN LED_MATRIX
         if(led_matrix_click_enable() < 0 ||
            led_matrix_click_set_intensity(1) < 0)
         {
             PRINTF("LED matrix init failed\n");
-            return 1;
+            PROCESS_EXIT();
         }
 
-        int flashes = 2;
+        led_matrix_click_set(dibujos[0]);
+
+        flashes = 2;
         while(flashes--) {
               /* Flash every second */
             for(i = 0; i < 20; i++)
@@ -303,6 +294,7 @@ PROCESS_THREAD(main_process, ev, data)
             leds_toggle(LED1);
         }
 
+        // CONFIGURACIÓN SOCKET UDP
         conn = udp_new_connection(PUERTO_CLIENTE, PUERTO_SERVIDOR, IP6_CI40);
         PROCESS_WAIT_UDP_CONNECTED();
         PROCESS_WAIT_UDP_CONNECTED();
@@ -324,10 +316,11 @@ PROCESS_THREAD(main_process, ev, data)
 
         PRINTF("Quien soy?\n");
 
+        // ENVIAR MENSAJE AL SERVIDOR PIDIENDO UN ID
         memset(operacion->data, '\0', MAXDATASIZE - ID_HEADER_LEN);
         strcpy(operacion->data, "Quien soy (MESA)");
         operacion->op = OP_WHOAMI_MESA;
-        operacion->id = (uint16_t) ((MESA_PREF + id_clicker) << 8) + id_msg;
+        operacion->id = (uint16_t) ((MESA_PREF + id_clicker) << 8) + id_msg;  // El primer byte se corresponde con el id de la mesa y el segundo con el nº de mensaje enviado
         operacion->len = strlen(operacion->data);
 
         PRINTF("(Enviado) OP: 0x%X ID: %ld Len: %ld Data: %s\n", operacion->op, operacion->id, operacion->len, operacion->data);
@@ -348,21 +341,22 @@ PROCESS_THREAD(main_process, ev, data)
         resultado = (struct idappdata*) &buffer;
 
         num_mesa= (uint8_t) strtol(resultado->data, NULL, 10);
-
-        id_clicker = (uint8_t) (MESA_PREF + num_mesa );
+        id_clicker = (uint8_t) (MESA_PREF + num_mesa);
 
         PRINTF("(Recibido) OP: 0x%X ID: %ld Len: %ld Data: %s\n", resultado->op, resultado->id, resultado->len, resultado->data);
 
+        // CONEXIÓN CON EL BROKER MQTT
         if (mqtt_register(&mqtt_conn, &main_process, &id_clicker, mqtt_event, MAXDATASIZE) < 0) {
             PRINTF("MQTT register failed\n");
-            return 1;
+            PROCESS_EXIT();
         }
 
         if (mqtt_connect(&mqtt_conn, IP6_CI40, 1883, 99) < 0) {
             PRINTF("MQTT connect failed\n");
-            return 1;
-        } 
+            PROCESS_EXIT();
+        }
 
+        // CREACIÓN DE LA MÁQUINA DE ESTADOS
         fsm = fsm_new(mesa_tt, id_clicker, id_msg, conn);
 
         PRINTF("********FSM CREADA********\n");
@@ -371,9 +365,11 @@ PROCESS_THREAD(main_process, ev, data)
 
         led_matrix_click_set(dibujos[4]);
 
+        // COMIENZO DEL BUCLE
         while(1)
         {
             etimer_set(&et, CLOCK_SECOND/2);
+
             button_callback();
             fsm_fire(fsm);
 
